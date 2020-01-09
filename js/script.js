1839840e0a2567be8e89c5ac59d90790bb679699
Luisa Logozzo <luisa.logozzo@gmail.com>
2020-01-09 18:49:11 +0100

    UP

--- js/script.js
+++ js/script.js
@@ -9,13 +9,14 @@ listaCognomi.push(cognomeUtente);
 var listaCognomiLower = [];
 for (var i = 0; i < listaCognomi.length; i++) {
 listaCognomiLower[i] = listaCognomi[i].toLowerCase();
-
 }
-
-// stampa la lista ordinata alfabeticamente (in html ul > li).
 listaCognomiLower.sort();
-console.log(listaCognomiLower);
-document.getElementById('lista').innerHTML = '<li>' + listaCognomiLower + '</li>';
+for (var i = 0; i < listaCognomiLower.length; i++) {
+  document.getElementById('lista').innerHTML += '<li>' + listaCognomiLower[i] + '</li>';
+}
+// stampa la lista ordinata alfabeticamente (in html ul > li).
+
+
 // Scrivi anche la posizione della lista in cui il nuovo utente si trova.
 listaCognomiLower.indexOf(cognomeUtente);
-console.log(listaCognomiLower.indexOf(cognomeUtente.toLowerCase()));
+document.getElementById('posizione').innerHTML = 'Posizione ' + listaCognomiLower.indexOf(cognomeUtente.toLowerCase());
